d4158f69f052e84541345edabada20cc18efacc8
kimjiin <kj980926@naver.com>
2022-04-12 21:53:52 +0900

    [#23] Design: Configure 개발자 소개 뷰

---
+++ SCON_iOS/Module/Home/DeveloperViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DeveloperViewController.swift
+//  SCON_iOS
+//
+//  Created by 김지인 on 2022/04/12.
+//
+
+import UIKit
+
+class DeveloperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
